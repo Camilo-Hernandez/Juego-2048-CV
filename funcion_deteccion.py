2400afe9c2919401d43ae377e864dfbbda70949e
Camilo Hernández Ruiz <camihruiz24@gmail.com>
2022-02-02 15:12:05 -0500

    Agregada la "detección del cambio de estado". Ahora se sabe cuándo entra y cuando sale de cada cuadro sin que se imprima en pantalla infinitamente

--- funcion_deteccion.py
+++ funcion_deteccion.py
@@ -1,10 +1,10 @@
 from platform import release
 import cv2
 import numpy as np
+
+
 colors = {'blue': (255, 0, 0), 'green': (0, 255, 0), 'red': (0, 0, 255), 'yellow': (60, 255, 255), 'magenta': (255, 0, 255), 'cyan': (255, 255, 0), 'white': (
     255, 255, 255), 'black': (0, 0, 0), 'gray': (125, 125, 125), 'rand': np.random.randint(0, high=256, size=(3,)).tolist(), 'dark_gray': (50, 50, 50), 'light_gray': (220, 220, 220)}
-
-cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 # Rango de color azul para detectarlo
 celesteBajo = np.array([75, 185, 88], np.uint8)
@@ -14,36 +14,41 @@ celesteAlto = np.array([112, 255, 255], np.uint8)
 x1 = None
 y1 = None
 
+posicion_actual = 'OUT'
+posicion_anterior = 'OUT'
+
 # Ciclo de video-streaming
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 while True:
     ret, frame = cap.read()
-    if ret == False: break
-    frame = cv2.flip(frame, 1) # Efecto espejo
+    if ret == False:
+        break
+    frame = cv2.flip(frame, 1)  # Efecto espejo
     frameHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Pasarlo de BGR a HSV
-    
+
     # ------------- Botones UP LEFT RIGHT DOWN ------------- #
     # -- Cuadrados dibujados en la parte inferior derecha que representan el movimiento del tablero -- #
-    
+
     # ------- Dibujar los rectángulos -------- #
-    ANCHO = 92
+    ANCHO = 100
 
     # Rectángulo arriba
-    UP_Px = 424
-    UP_Py = 216
+    UP_Px = 275
+    UP_Py = 10
     UP_P = (UP_Px, UP_Py)
     UP_LONG = (UP_Px+ANCHO, UP_Py+ANCHO)
     cv2.rectangle(frame, UP_P, UP_LONG, colors['yellow'], 2)
 
     # Rectángulo abajo
     DOWN_Px = UP_Px
-    DOWN_Py = UP_Py + 168
+    DOWN_Py = UP_Py + 268
     DOWN_P = (DOWN_Px, DOWN_Py)
     DOWN_LONG = (DOWN_Px+ANCHO, DOWN_Py+ANCHO)
     cv2.rectangle(frame, DOWN_P, DOWN_LONG, colors['yellow'], 2)
 
     # Rectángulo izquierda
-    LEFT_Px = UP_Px - 113
-    LEFT_Py = UP_Py + 86
+    LEFT_Px = 30
+    LEFT_Py = 140
     LEFT_P = (LEFT_Px, LEFT_Py)
     LEFT_LONG = (LEFT_Px+ANCHO, LEFT_Py+ANCHO)
     cv2.rectangle(frame, LEFT_P, LEFT_LONG, colors['yellow'], 2)
@@ -54,7 +59,6 @@ while True:
     RIGHT_P = (RIGHT_Px, RIGHT_Py)
     RIGHT_LONG = (RIGHT_Px+ANCHO, RIGHT_Py+ANCHO)
     cv2.rectangle(frame, RIGHT_P, RIGHT_LONG, colors['yellow'], 2)
-
 
     # ---- Dibujar flechas ---- #
     # Flecha arriba
@@ -96,7 +100,7 @@ while True:
     # ------------- Detección del color celeste y obtención de la imagen binaria ------------- #
     # Detección del color celeste y limpiar el ruido del objeto con transformaciones marfológicas
     maskCeleste = cv2.inRange(frameHSV, celesteBajo, celesteAlto)
-    maskCeleste = cv2.erode(maskCeleste, None, iterations=1) 
+    maskCeleste = cv2.erode(maskCeleste, None, iterations=1)
     maskCeleste = cv2.dilate(maskCeleste, None, iterations=2)
     maskCeleste = cv2.medianBlur(maskCeleste, 13)
     # Obtener el contorno de los objetos para reconocer el más grande (la tapa o lapicero azul)
@@ -113,35 +117,50 @@ while True:
         else:
             # Dibujar un rectángulo alrededor del lapicero (area blanca de la imagen binaria)
             x, y2, w, h = cv2.boundingRect(c)
-            cv2.rectangle(frame,(x,y2),(x+w,y2+h),colors['green'],2)
+            cv2.rectangle(frame, (x, y2), (x+w, y2+h), colors['green'], 2)
             x2 = x + w//2
 
             if x1 is not None:
                 # Si detecta la punta del lapicero en UP
                 if x2 > UP_P[0] and y2 > UP_P[1] and x2 < UP_LONG[0] and y2 < UP_LONG[1]:
-                    print('está en UP')
+                    posicion_actual = 'IN'
+                    if (posicion_actual != posicion_anterior):
+                        print('está en UP')
+                        key = 'w'
                 # # Si detecta la punta del lapicero en DOWN
-                if x2 > DOWN_P[0] and y2 > DOWN_P[1] and x2 < DOWN_LONG[0] and y2 < DOWN_LONG[1]:
-                    print('está en DOWN')
+                elif x2 > DOWN_P[0] and y2 > DOWN_P[1] and x2 < DOWN_LONG[0] and y2 < DOWN_LONG[1]:
+                    posicion_actual = 'IN'
+                    if (posicion_actual != posicion_anterior):
+                        print('está en DOWN')
+                        key = 's'
                 # # Si detecta la punta del lapicero en LEFT
-                if x2 > LEFT_P[0] and y2 > LEFT_P[1] and x2 < LEFT_LONG[0] and y2 < LEFT_LONG[1]:
-                    print('está en LEFT')
+                elif x2 > LEFT_P[0] and y2 > LEFT_P[1] and x2 < LEFT_LONG[0] and y2 < LEFT_LONG[1]:
+                    posicion_actual = 'IN'
+                    if (posicion_actual != posicion_anterior):
+                        print('está en LEFT')
+                        key = 'a'
                 # # Si detecta la punta del lapicero en RIGHT
-                if x2 > RIGHT_P[0] and y2 > RIGHT_P[1] and x2 < RIGHT_LONG[0] and y2 < RIGHT_LONG[1]:
-                    print('está en RIGHT')
+                elif x2 > RIGHT_P[0] and y2 > RIGHT_P[1] and x2 < RIGHT_LONG[0] and y2 < RIGHT_LONG[1]:
+                    posicion_actual = 'IN'
+                    if (posicion_actual != posicion_anterior):
+                        print('está en RIGHT')
+                        key = 'd'
+                else:
+                    posicion_actual = 'OUT'
+                    if (posicion_actual != posicion_anterior):
+                        print('está OUT')
+
             cv2.circle(frame, (x2, y2), 2, colors['cyan'], 3)
             x1 = x2
             y1 = y2
+            posicion_anterior = posicion_actual
 
-                
-
-
-
-
-    cv2.imshow('maskCeleste', maskCeleste)
+    # cv2.imshow('maskCeleste', maskCeleste)
     cv2.imshow('Frame', frame)
     k = cv2.waitKey(1)
-    if k == 27: break # abortar con ESC
+    if k == 27:
+        break  # abortar con ESC
 
 cap.release()
 cv2.destroyAllWindows
+
